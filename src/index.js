2c773797f42086a3d4b6e81a71e7afbd9c580534
Denis Danilkov <kvantokost@gmail.com>
2019-10-06 10:02:08 +0300

    feat: do all tests passed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 module.exports = function multiply(first, second) {
+
   let firstMultiplier = first.split('').reverse();
   let secondMultiplier = second.split('');
+
   let interimProductsArray = [];
   let remainderDecimal;
 
-  for (let i = secondMultiplier.length - 1; i >= 0; i--) {
+  for (let i = secondMultiplier.length - 1; i >= 0; i--) { //make products of multiplication
     remainderDecimal = 0;
     let currentProduct = firstMultiplier.reduce((product, number, index, array) => {
       if ((number * secondMultiplier[i] + remainderDecimal) < 10) {
@@ -13,7 +15,10 @@ module.exports = function multiply(first, second) {
       }
       else {
         if (index == array.length - 1) {
-          product.push((String(number * secondMultiplier[i] + remainderDecimal)).split('').reverse().join(''));
+          if ((String(number * secondMultiplier[i] + remainderDecimal).length > 1)) {
+            product.push((String(number * secondMultiplier[i] + remainderDecimal)).split('').reverse().join(''));
+          }
+          else { product.push(String(number * secondMultiplier[i] + remainderDecimal)) }
           remainderDecimal = 0;
         }
         else {
@@ -23,27 +28,41 @@ module.exports = function multiply(first, second) {
       }
       return product;
     }, [])
-    interimProductsArray.push(currentProduct.join('').split('')); /*добавляем каждое получившееся произведение*/
+    interimProductsArray.push(currentProduct.join('').split(''));
   }
 
   let startAddingIndex = 1;
   let interimAddArray = [];
   let additionProductsNumbers;
 
-  while (interimProductsArray.length > 1) {
+  while (interimProductsArray.length > 1) { // add up all products
     for (let i = 0; i < interimProductsArray.length; i++) {
-      let resultProduct = interimProductsArray[0];
-      let currentProduct = interimProductsArray[1];
 
       if (i == 1) {
+        let resultProduct = interimProductsArray[0];
+        let currentProduct = interimProductsArray[i];
+
         for (let j = 0; j < startAddingIndex; j++) {
           interimAddArray.push(resultProduct[j]);
         }
+
         for (let u = 0; u < currentProduct.length; u++) {
 
           if (!resultProduct[startAddingIndex + u]) {
-            interimAddArray.push(String(Number(currentProduct[u]) + remainderDecimal));
-            continue;
+
+            if ((Number(currentProduct[u]) + remainderDecimal) < 10) {
+              interimAddArray.push(String(Number(currentProduct[u]) + remainderDecimal));
+              remainderDecimal = 0;
+              continue;
+            }
+            else {
+              interimAddArray.push(String((Number(currentProduct[u]) + remainderDecimal) % 10));
+              remainderDecimal = ((Number(currentProduct[u]) + remainderDecimal) - (Number(currentProduct[u]) + remainderDecimal) % 10) / 10;
+              if (u == (currentProduct.length - 1)) {
+                interimAddArray.push(String(remainderDecimal))
+              }
+              continue;
+            }
           }
 
           additionProductsNumbers = Number(currentProduct[u]) + Number(resultProduct[startAddingIndex + u]) + remainderDecimal; /*здесь ошибка с Nan вылазит когда индекс resultProduct просим которого у него нет*/
@@ -56,6 +75,11 @@ module.exports = function multiply(first, second) {
             interimAddArray.push(String(additionProductsNumbers % 10));
             remainderDecimal = (additionProductsNumbers - (additionProductsNumbers % 10)) / 10;
           }
+
+          if (u == (currentProduct.length - 1) && remainderDecimal > 0) {
+            interimAddArray.push(String(remainderDecimal));
+            remainderDecimal = 0;
+          }
         }
       }
     }
@@ -67,6 +91,6 @@ module.exports = function multiply(first, second) {
   }
 
   return interimProductsArray.reduce((finalString, element, index, array) => {
-  return finalString + element.reverse().join('');
+    return finalString + element.reverse().join('');
   }, '');
 }
